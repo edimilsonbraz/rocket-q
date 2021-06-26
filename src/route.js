34a0dfdd2f5732b9143179c9a8c15715d0f903c7
Edimilson Braz <edimilsonbraz@pop-os.localdomain>
2021-06-26 17:02:30 -0300

    CRUD no SQLITE

--- src/route.js
+++ src/route.js
@@ -7,13 +7,23 @@ const route = express.Router();
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+
+route.post('/question/create/:room', QuestionController.create)
 
 // Informações do form vem por essa rota
 route.post('/question/:room/:question/:action', QuestionController.index)
-route.post('/create-room', RoomController.create)
+
+
 
 module.exports = route
+
+
+
+
 
 // METODO "POST" traz parametros dentro da rota vindas de um form no html
 // "action" tem a rota que traz esses parametros
